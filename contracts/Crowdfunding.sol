2623bc0a4bf4b3722f7d98118e31eb9dc77f5bdd
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-04-19 09:35:40 +0800

    Add logic to set campaign status 'isComplete' to true

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -4,8 +4,24 @@ pragma solidity ^0.4.17;
 contract Crowdfunding {
     address[] public campaigns;
 
-    function createCampaign(string memory _image, string memory _title, string memory _description, uint _target, uint _endDate) public {
-        address newCampaign = new Campaign(_image, _title, _description, _target, _endDate, msg.sender);
+    function createCampaign(
+        string memory _image,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _endDate
+    ) public {
+        require(_endDate <= now);
+        require(_target <= 0);
+
+        address newCampaign = new Campaign(
+            _image,
+            _title,
+            _description,
+            _target,
+            _endDate,
+            msg.sender
+        );
         campaigns.push(newCampaign);
     }
 
@@ -18,28 +34,35 @@ contract Campaign {
     string public image;
     string public title;
     string public description;
-    uint public balance;
-    uint public target;
-    uint public endDate;
+    uint256 public balance;
+    uint256 public target;
+    uint256 public endDate;
     bool public isComplete;
     address public creatorAddress;
     Contribute[] public contributors;
 
     struct Contribute {
-        uint amount;
+        uint256 amount;
         address contributor;
     }
 
     modifier onlyCreator() {
-        require (msg.sender == creatorAddress);
+        require(msg.sender == creatorAddress);
         _;
     }
 
-    function Campaign(string memory _image, string memory _title, string memory _description, uint _target, uint _endDate, address _creatorAddress) public {
+    function Campaign(
+        string memory _image,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _endDate,
+        address _creatorAddress
+    ) public {
         image = _image;
-        title =  _title;
-        description =  _description;
-        target =  _target;
+        title = _title;
+        description = _description;
+        target = _target;
         endDate = _endDate;
         creatorAddress = _creatorAddress;
     }
@@ -47,7 +70,6 @@ contract Campaign {
     // TODO: Tambah paramter untuk menambahkan pesan
     function contribute() public payable {
         require(isComplete == false);
-        require(now <= endDate);
         require(msg.value > 0);
 
         Contribute memory newContribute = Contribute({
@@ -57,17 +79,39 @@ contract Campaign {
 
         contributors.push(newContribute);
         balance += msg.value;
+
+        if (balance >= target) {
+            isComplete = true;
+        }
+        if (now > endDate) {
+            isComplete = true;
+        }
     }
 
-    function deliverBalance(uint _value) onlyCreator public payable {
+    function deliverBalance(uint256 _value) public payable onlyCreator {
         require(balance > 0);
         require(_value > 0);
 
         creatorAddress.transfer(_value);
         isComplete = true;
     }
-    
-    function getCampaign() public view returns (string memory, string memory, string memory, uint, uint, uint, uint, bool, address, address) {
+
+    function getCampaign()
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            bool,
+            address,
+            address
+        )
+    {
         return (
             image,
             title,
@@ -82,18 +126,23 @@ contract Campaign {
         );
     }
 
-    function getContributors() public view returns (address[] memory, uint[] memory) {
-        address[] memory contributorsAddress = new address[](contributors.length);
-        uint[] memory contributorsAmount = new uint[](contributors.length);
+    function getContributors()
+        public
+        view
+        returns (address[] memory, uint256[] memory)
+    {
+        address[] memory contributorsAddress = new address[](
+            contributors.length
+        );
+        uint256[] memory contributorsAmount = new uint256[](
+            contributors.length
+        );
 
-        for (uint i = 0; i < contributors.length; i++) {
+        for (uint256 i = 0; i < contributors.length; i++) {
             contributorsAddress[i] = contributors[i].contributor;
             contributorsAmount[i] = contributors[i].amount;
         }
 
-        return (
-            contributorsAddress,
-            contributorsAmount
-        );
+        return (contributorsAddress, contributorsAmount);
     }
 }
